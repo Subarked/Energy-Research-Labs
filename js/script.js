954de062f5b4118ecfdaa3a34dabc21067ebc086
Subarked <68792391+Subarked@users.noreply.github.com>
2022-02-06 19:00:23 -0600

    Batch update

---
+++ js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":1199},"pages":[{"id":"page-home-page","displayName":"Home Page","link":{"linkType":"LinkTypePage","href":"#!page-home-page"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-anomalies-hub","displayName":"Anomalies Hub","link":{"linkType":"LinkTypePage","href":"#!page-anomalies-hub"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
